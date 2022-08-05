cded16859acb8575896081639f83d7b71f908098
Big bank <6310110234@psu.ac.th>
2022-08-05 19:35:18 +0700

    Part4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
+const word = "Hello";
 function App() {
   return (
     <div>
-      Hello{"World"}
+      {
+        <WordCard value="hello"/>
+      }
+        
     </div>
   );
 }
