9ce69250c06836a4eaa4311265dad4b510d24a82
Big bank <6310110234@psu.ac.th>
2022-08-11 23:49:09 +0700

    Part7_2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
 
-const word = "Hello";
+var quizz = ['hello', 'super', 'memory', 'kamui'];
+var quizshow = quizz[Math.floor(Math.random()*quizz.length)];
+
+// const word = "Hello";
 function App() {
   return (
-    <div>
-      {
-        <WordCard value="hello"/>
-      }
-        
+    <div className = 'box'>
+      <div className = 'background'>
+        <WordCard value={quizshow}/>
+      </div>
     </div>
   );
 }
--- submission.md
+++ submission.md
@@ -4,3 +4,8 @@ Commit and Push Part7_1
     - ใน App.css เปลี่ยนขนาดตัวอักษรเป็น 3 em
     - ใน App.css เปลี่ยนตัวอักษรเป็นสีนํ้าเงิน
 
+Commit and Push Part7_2
+    - ใน App.js เพิ่มตัวแปรชื่อ quizz ที่เป็นชนิดอาเรย์กับชื่อความ 4 ข้อความ
+    - ใน App.js fuction App() เพิ่ม syntax ที่จะสุ่มค่าข้อความในตัวแปร quizz
+    - ใน App.js fuction App() ตกแต่งกล่องข้อความโดยการเพิ่ม box, button, background ลงใน App.css ซึ่งเชื่อมยู่กับ App.js
+    
